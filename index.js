2daa1d8c0fcb60067fc237ddc0f9bc3b7b55f2e6
Pavlina Glozygova <pglozygova@gmail.com>
2023-03-09 19:19:37 +0100

    modified:   index.js

--- index.js
+++ index.js
@@ -27,3 +27,14 @@ const strongPassword = (len) => {
 
   return result;
 };
+
+const createAccount = (user, generatePassword) => {
+  return `Uživatel "${user}" s heslem "${generatePassword(9)}" `
+}
+
+document.body.innerHTML += `
+	<p>${createAccount('Míša', weakPassword)}</p>
+	<p>${createAccount('Řízek', mediumPassword)}</p>
+	<p>${createAccount('Hustodémon', strongPassword)}</p>
+`
+
